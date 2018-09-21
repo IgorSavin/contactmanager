fc00f8780efc685436ac3d6e0dc04200eb160604
Igor Savin <beixiaofeng@gmail.com>
2018-09-21 10:51:58 +0300

    created add function

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Consumer} from '../../context';
+import uuid from 'uuid';
 
 class AddContact extends Component {
   state = {
@@ -7,9 +9,23 @@ class AddContact extends Component {
     phone: ''
   };
 
-  onSubmit = (e)=>{
+  onSubmit = (dispatch, e)=>{
     e.preventDefault();
-    console.log(this.state);
+    const {name, email, phone} = this.state;
+
+    const newContact ={
+      id:uuid(),
+      name,
+      email,
+      phone
+    }
+    dispatch({type: 'ADD_CONTACT', payload:newContact});
+
+    this.setState({
+      name:'',
+      email:'',
+      phone:''
+    })
   }
 
   onChange = (e) =>this.setState({[e.target.name]:e.target.value});
@@ -17,11 +33,15 @@ class AddContact extends Component {
   render() {
     const {name, email, phone}=this.state;
 
-    return (
-      <div className="card mb-3">
+    return(
+      <Consumer>
+        {value=>{
+          const {dispatch} = value;
+          return(
+            <div className="card mb-3">
         <div className="card-header">Add Contact</div>
         <div className="card-body">
-          <form onSubmit={this.onSubmit}>
+          <form onSubmit={this.onSubmit.bind(this,dispatch)}>
             <div className="form-group">
               <label htmlFor="name">Name</label>
               <input
@@ -63,6 +83,9 @@ class AddContact extends Component {
           </form>
         </div>
       </div>
+          )
+        }}
+      </Consumer>
     )
   }
 }
