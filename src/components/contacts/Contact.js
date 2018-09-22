4d79430cd8937f4273f45bbf2905eda979b8e1cb
Igor Savin <beixiaofeng@gmail.com>
2018-09-22 17:19:35 +0300

    full fanctioning crud app

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Consumer } from '../../context';
+import {Link} from 'react-router-dom';
 import axios from 'axios';
 
 class Contact extends Component {
@@ -8,9 +9,13 @@ class Contact extends Component {
     showContactInfo: false
   }
 
-  onDeleteClick = (id, dispatch) => {
-    axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`)
-    .then(res=>dispatch({ type: 'DELETE_CONTACT', payload: id }))
+  onDeleteClick = async (id, dispatch) => {
+    try{
+    await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+    dispatch({ type: 'DELETE_CONTACT', payload: id });
+    }catch(e){
+      dispatch({ type: 'DELETE_CONTACT', payload: id });
+    }
    
   };
 
@@ -44,6 +49,15 @@ class Contact extends Component {
                   }}
                   onClick={this.onDeleteClick.bind(this, id, dispatch)}
                 />
+                <Link to={`contact/edit/${id}`}>
+                <i className="fas fa-pencil-alt"
+                style={{
+                  cursor:'pointer',
+                  float:'right',
+                  color:'black',
+                  marginRight: '1rem'
+                }}/>
+                </Link>
               </h4>
               {showContactInfo ? (
                 <ul className="list-group">
--- src/context.js
+++ src/context.js
@@ -1,28 +1,40 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from 'react'
+import axios from 'axios'
 
-const Context = React.createContext();
+const Context = React.createContext()
 
 const reducer = (state, action) => {
-  switch(action.type){
+  switch (action.type) {
     case 'DELETE_CONTACT':
-    return{
-      ...state,
-      contacts: state.contacts.filter(contact => contact.id !== action.payload)
-    }
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+        )
+      }
     case 'ADD_CONTACT':
-    return{
-      ...state,
-      contacts: [action.payload,...state.contacts]
-    }
+      return {
+        ...state,
+        contacts: [action.payload, ...state.contacts]
+      }
+    case 'UPDATE_CONTACT':
+      return {
+        ...state,
+        contacts: state.contacts.map(
+          contact =>
+            contact.id === action.payload.id
+              ? (contact = action.payload)
+              : contact
+        )
+      }
     default:
-    return state;
+      return state
   }
 }
 
 export class Provider extends Component {
   state = {
-    contacts:[
+    contacts: [
       // {
       //   id:1,
       //   name:'Igor Savin',
@@ -42,21 +54,20 @@ export class Provider extends Component {
       //   phone:'098994045'
       // }
     ],
-    dispatch: action => this.setState(state =>reducer(state,action))
-  };
-  async componentDidMount(){
-    const res = await axios
-    .get('https://jsonplaceholder.typicode.com/users');
-    this.setState({contacts:res.data});
+    dispatch: action => this.setState(state => reducer(state, action))
+  }
+  async componentDidMount() {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/users')
+    this.setState({ contacts: res.data })
   }
 
   render() {
     return (
       <Context.Provider value={this.state}>
-      {this.props.children}
+        {this.props.children}
       </Context.Provider>
-    );
+    )
   }
 }
 
-export const Consumer = Context.Consumer;
+export const Consumer = Context.Consumer
