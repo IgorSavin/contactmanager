06d0399fd049e1d29e58cf50439b3d3b757c7d06
Igor Savin <beixiaofeng@gmail.com>
2018-09-22 12:00:14 +0300

    added axios

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 const Context = React.createContext();
 
@@ -22,27 +23,32 @@ const reducer = (state, action) => {
 export class Provider extends Component {
   state = {
     contacts:[
-      {
-        id:1,
-        name:'Igor Savin',
-        email:'beixiaofeng@gmail.com',
-        phone:'0961872044'
-      },
-      {
-        id:2,
-        name:'Dima Didenko',
-        email:'didko@gmail.com',
-        phone:'0992889035'
-      },
-      {
-        id:3,
-        name:'Alexander Babenko',
-        email:'abeb@gmail.com',
-        phone:'098994045'
-      }
+      // {
+      //   id:1,
+      //   name:'Igor Savin',
+      //   email:'beixiaofeng@gmail.com',
+      //   phone:'0961872044'
+      // },
+      // {
+      //   id:2,
+      //   name:'Dima Didenko',
+      //   email:'didko@gmail.com',
+      //   phone:'0992889035'
+      // },
+      // {
+      //   id:3,
+      //   name:'Alexander Babenko',
+      //   email:'abeb@gmail.com',
+      //   phone:'098994045'
+      // }
     ],
     dispatch: action => this.setState(state =>reducer(state,action))
   };
+  async componentDidMount(){
+    const res = await axios
+    .get('https://jsonplaceholder.typicode.com/users');
+    this.setState({contacts:res.data});
+  }
 
   render() {
     return (
